9c49d6326b1161aa2d5cba44325b08aa30b207f8
Kate Henderson <hi@kate.nz>
2016-04-18 14:47:39 +1200

    Initial commit of Unity project

---
+++ AssetProj/TestAssets/Assets/cam.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cam : MonoBehaviour {
+
+	public Transform myCamera;
+	public Transform target;
+	// Use this for initialization
+	void Start () {
+		myCamera = GetComponent<Transform>();
+		target = GetComponent<Transform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		print (target.position);
+		print (myCamera.position);
+		myCamera.position = target.position;// + new Vector3 (0, 10, 0);
+	}
+}
---
+++ AssetProj/TestAssets/Assets/move.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour {
+	public Transform ship;
+	private Vector3 forwardVec;
+	// Use this for initialization
+	void Start () {
+		ship = GetComponent<Transform>();
+		forwardVec = Vector3.forward;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ship.Translate(Vector3.forward);
+		if(Input.GetKey(KeyCode.Q)){
+			ship.Translate(Vector3.left*3);
+		}
+		if(Input.GetKey(KeyCode.E)){
+			ship.Translate(Vector3.right*3);
+		}
+		if(Input.GetKey(KeyCode.A)){
+			ship.Rotate(Vector3.down);
+		}
+		if(Input.GetKey(KeyCode.D)){
+			ship.Rotate(Vector3.up);
+		}
+		if(Input.GetKey(KeyCode.W)){
+			ship.Translate(Vector3.up);
+		}
+		if(Input.GetKey(KeyCode.S)){
+			ship.Translate(Vector3.down);
+		}
+//		if(Input.GetKey(KeyCode.LeftShift)){
+//			forwardVec = forwardVec * 1.1;
+//		}
+//		if(Input.GetKey(KeyCode.LeftControl)){
+//			forwardVec = forwardVec * 0.9;
+//		}
+	}
+}
