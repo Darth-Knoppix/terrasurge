975d60ce6ac51cf66310e6ef94cc5f6dbc43faee
Seth Corker <seth.corker@gmail.com>
2016-05-14 15:48:36 +1200

    Animated Obstacle Test
    Added a scene to test animating obstacles using the animator and
    scripting. Mapped to 'Q' for testing.

---
+++ TerraSurge/Assets/GroundObstacleController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundObstacleController : MonoBehaviour {
+	Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = this.GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Q)){
+			print ("FF");
+			animator.SetBool("trig", !animator.GetBool("trig"));
+		}
+	}
+}
