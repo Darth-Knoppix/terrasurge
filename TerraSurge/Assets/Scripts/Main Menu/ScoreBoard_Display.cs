630ec1459ebd5833c211bc8f3971f4778f78a511
gittyaboutgit <dark_frezzer@hotmail.com>
2016-06-13 18:42:30 +1200

    Updated high score script

--- TerraSurge/Assets/Scripts/Main Menu/ScoreBoard_Display.cs
+++ TerraSurge/Assets/Scripts/Main Menu/ScoreBoard_Display.cs
@@ -3,17 +3,52 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class ScoreBoard_Display : MonoBehaviour {
-	private Text currentScore;
-	private ScoreController scoreboard;
 
-	// Use this for initialization
-	void Start () {
-		//currentScore = GameObject.Find ("HighScore_Value").GetComponent<Text> ();
-		//scoreboard = GameObject.Find("GameManager").GetComponent<ScoreController> ();
-	}
+    public GameObject score1;
+    public GameObject score2;
+    public GameObject score3;
+    public GameObject score4;
+    public GameObject score5;
+
+    private bool scoresShown;
+
+    // Use this for initialization
+    void Start () {
+        score1.SetActive(false);
+        score2.SetActive(false);
+        score3.SetActive(false);
+        score4.SetActive(false);
+        score5.SetActive(false);
+
+        scoresShown = false;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		//currentScore.text = scoreboard.getScoref ();
+
 	}
+
+    public void toggleHighScores ()
+    {
+        if (scoresShown)
+        {
+            score1.SetActive(false);
+            score2.SetActive(false);
+            score3.SetActive(false);
+            score4.SetActive(false);
+            score5.SetActive(false);
+
+            scoresShown = false;
+        }
+        else
+        {
+            score1.SetActive(true);
+            score2.SetActive(true);
+            score3.SetActive(true);
+            score4.SetActive(true);
+            score5.SetActive(true);
+
+            scoresShown = true;
+        }
+    }
 }
